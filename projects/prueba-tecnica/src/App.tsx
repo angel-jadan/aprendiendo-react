c97b58971c4ff9e618f152ee95bb6654a4a62060
Angel <angel_jadan@outlook.com>
2023-11-19 11:39:16 -0500

    test complete

--- projects/prueba-tecnica/src/App.tsx
+++ projects/prueba-tecnica/src/App.tsx
@@ -1,26 +1,115 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 
-import { type User } from './helpers/types/users'
+import { type User } from "./types";
 import { UsersList } from "./components/UsersList";
+
+enum SortBy {
+  NONE = "none",
+  NAME = "name",
+  LAST = "last",
+  COUNTRY = "country",
+}
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [showColors, setShowColors] = useState<boolean>(false);
+  const [sortBy, setSortBy] = useState<SortBy>(SortBy.NONE);
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
       .then(async (res) => await res.json())
       .then((res) => {
         setUsers(res.results);
+        originalUsers.current = res.results;
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  // use Ref -> para guardar un estado
+  // que queremos que se comprata entre renderizados
+  // pero al cambiar, no vuelva a renderizar el componente
+  const originalUsers = useRef<User[]>([]);
+
+  const toggleColors = () => {
+    setShowColors(!showColors);
+  };
+
+  const toggleSortByCountry = () => {
+    const sortCountry = SortBy.NONE === sortBy ? SortBy.COUNTRY : SortBy.NONE
+    setSortBy(sortCountry);
+  };
+
+  const handleDeleteUser = (email: string) => {
+    const filterListUser = users.filter((user) => user.email !== email);
+    setUsers(filterListUser);
+  };
+
+  const handleResetState = () => {
+    setUsers(originalUsers.current);
+  };
+
+  const filteredUsers = useMemo(() => {
+    return filterCountry !== null && filterCountry?.length > 0
+      ? users.filter((user) =>
+          user.location.country
+            .toLowerCase()
+            .includes(filterCountry.toLowerCase())
+        )
+      : users;
+  }, [users, filterCountry]);
+
+  const sortedUsers = useMemo(() => {
+    if (sortBy === SortBy.NONE) return filteredUsers;
+    const compareProperties: Record<string, (user: User) => any> = {
+      [SortBy.NAME]: user => user.name.first,
+      [SortBy.LAST]: user => user.name.last,
+      [SortBy.COUNTRY]: user => user.location.country,
+    }
+    return filteredUsers.toSorted((a, b) => {
+      const extractProperties = compareProperties[sortBy]
+      return extractProperties(a).localeCompare(extractProperties(b));
+    })
+  }, [filteredUsers, sortBy]);
+
+
+  const handleChangeSort = (sort: SortBy) => {
+    if (sortBy === sort) {
+      setSortBy(SortBy.NONE)      
+    } else {
+      setSortBy(sort)
+    }
+  }
+
+
   return (
     <>
       <h1>Prueba técnica</h1>
-      <UsersList users={users} />
+      <header>
+        <button onClick={toggleColors}>Show Colors</button>
+        <button onClick={toggleSortByCountry}>
+          {sortBy === SortBy.COUNTRY ? "No ordenar por País" : "Ordenar por país"}
+        </button>
+        <button onClick={handleResetState}>Resetear estado</button>
+        <input
+          type="text"
+          placeholder="Filtrar por país"
+          onChange={(event) => {
+            setFilterCountry(event.target.value);
+          }}
+        />
+      </header>
+      <main>
+        <UsersList
+          users={sortedUsers}
+          showColors={showColors}
+          deleteUser={handleDeleteUser}
+          changeSort={handleChangeSort}
+        />
+      </main>
     </>
   );
 }
--- projects/prueba-tecnica/src/components/UsersList.tsx
+++ projects/prueba-tecnica/src/components/UsersList.tsx
@@ -1,25 +1,44 @@
-import { type User } from "../helpers/types/users";
+import { type User } from "../types";
+
+declare global {
+  interface Array<T> {
+    toSorted(compareFn?: (a: T, b: T) => number): T[];
+  }
+}
+
+enum SortBy {
+  NONE = "none",
+  NAME = "name",
+  LAST = "last",
+  COUNTRY = "country",
+}
 
 interface Props {
   users: User[];
+  showColors: boolean;
+  deleteUser: (email: string) => void;
+  changeSort: (sort: SortBy) => void
 }
 
-export function UsersList({ users }: Props) {
+export function UsersList({ users, showColors, deleteUser, changeSort }: Props) {
   return (
     <table width="100%">
       <thead>
         <tr>
           <th>Foto</th>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>País</th>
+          <th onClick={() => changeSort(SortBy.NAME)}>Nombre</th>
+          <th onClick={() => changeSort(SortBy.LAST)}>Apellido</th>
+          <th onClick={() => changeSort(SortBy.COUNTRY)}>País</th>
           <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user: User) => {
+        {users.map((user: User, index: number) => {
+          const backgroudColor = index % 2 === 0 ? "#3333" : "#555";
+          const color = showColors ? backgroudColor : "transparent";
+
           return (
-            <tr key={user.email}>
+            <tr key={user.email} style={{ background: color }}>
               <td>
                 <img src={user.picture.thumbnail}></img>
               </td>
@@ -27,7 +46,7 @@ export function UsersList({ users }: Props) {
               <td>{user.name.last}</td>
               <td>{user.location.country}</td>
               <td>
-                <button>Eliminar</button>
+                <button onClick={() => deleteUser(user.email)}>Eliminar</button>
               </td>
             </tr>
           );
